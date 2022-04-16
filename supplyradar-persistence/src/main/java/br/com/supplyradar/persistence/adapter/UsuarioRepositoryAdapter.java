08be1cc5d0faec0a66765f5a9d8b32e6381b3f46
Mychell Teixeira <mychellt@gmail.com>
2022-04-16 02:16:16 -0300

    Fix: Correcao no teste de unidade de cadastro de usuario.

--- supplyradar-persistence/src/main/java/br/com/supplyradar/persistence/adapter/UsuarioRepositoryAdapter.java
+++ supplyradar-persistence/src/main/java/br/com/supplyradar/persistence/adapter/UsuarioRepositoryAdapter.java
@@ -2,14 +2,20 @@ package br.com.supplyradar.persistence.adapter;
 
 import br.com.supplyradar.core.persistence.UsuarioRepository;
 import br.com.supplyradar.domain.commons.Usuario;
+import br.com.supplyradar.persistence.adapter.jpa.UsuarioJpaRepository;
+import br.com.supplyradar.persistence.mapper.UsuarioEntityMapper;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+@AllArgsConstructor
 @Repository
 public class UsuarioRepositoryAdapter implements UsuarioRepository {
+    private final UsuarioJpaRepository jpa;
+    private final UsuarioEntityMapper mapper;
 
     @Override
     public Usuario save(Usuario usuario) {
-        return null;
+        return mapper.mapFrom(jpa.save(mapper.mapFrom(usuario)));
     }
 
     @Override
--- supplyradar-persistence/src/test/java/br/com/supplyradar/persistence/adapter/SolicitacaoAcessoRepositoryAdapterTest.java
+++ supplyradar-persistence/src/test/java/br/com/supplyradar/persistence/adapter/SolicitacaoAcessoRepositoryAdapterTest.java
@@ -3,8 +3,10 @@ package br.com.supplyradar.persistence.adapter;
 import br.com.six2six.fixturefactory.Fixture;
 import br.com.supplyradar.core.persistence.SolicitacaoAcessoRepository;
 import br.com.supplyradar.core.persistence.TokenSolicitacaoAcessoRepository;
+import br.com.supplyradar.core.persistence.UsuarioRepository;
 import br.com.supplyradar.domain.commons.SolicitacaoAcesso;
 import br.com.supplyradar.domain.commons.TokenSolicitacaoAcesso;
+import br.com.supplyradar.domain.commons.Usuario;
 import br.com.supplyradar.persistence.AbstractPersistenceTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -18,12 +20,23 @@ public class SolicitacaoAcessoRepositoryAdapterTest extends AbstractPersistenceT
     @Autowired
     private SolicitacaoAcessoRepository repository;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @DisplayName(value = "Deve realizar a pesistência da solicitação de acesso")
     @Test
     void save() {
         final SolicitacaoAcesso solicitacao = Fixture.from(SolicitacaoAcesso.class).gimme("valido");
+        final Usuario usuario = Fixture.from(Usuario.class).gimme("valido");
+        Usuario usuarioPersistido = usuarioRepository.save(usuario);
+        solicitacao.setUsuario(usuarioPersistido);
+
         final SolicitacaoAcesso solicitacaoPersistida = repository.save(solicitacao);
         assertThat(solicitacaoPersistida, notNullValue());
         assertThat(solicitacaoPersistida.getId(), notNullValue());
+        assertThat(solicitacaoPersistida.getUsuario(), notNullValue());
+        assertThat(solicitacaoPersistida.getUsuario().getId(), notNullValue());
+        assertThat(solicitacaoPersistida.getUsuario().getPessoa(), notNullValue());
+        assertThat(solicitacaoPersistida.getUsuario().getPessoa().getId(), notNullValue());
     }
 }
